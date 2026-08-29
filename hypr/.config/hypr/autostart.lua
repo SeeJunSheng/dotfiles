420b488f791221599dca3f0e7a49c81d42f640a6
Jun <212102983+SeeJunSheng@users.noreply.github.com>
2026-08-30 01:23:53 +0800

    feat(hypr): autostart quickshell

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -3,6 +3,7 @@ local function start_services()
 	hl.exec_cmd("hypridle")
 	hl.exec_cmd("wl-paste --watch cliphist store")
 	hl.exec_cmd("systemctl --user start hyprpolkitagent.service")
+	hl.exec_cmd("qs")
 end
 
 hl.on("hyprland.start", start_services)
